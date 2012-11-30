6df6bcce3b9ea8e328f114e751bde7cbfc7cc26f
Christos Papachristou <tsoupap@gmail.com>
2012-11-30 21:37:31 +0100

    tcpvideosource: added wrapper and systemd service file

--- poky/meta-wpss/recipes/tcpvideosource/files/videosrcloop.sh
+++ poky/meta-wpss/recipes/tcpvideosource/files/videosrcloop.sh
@@ -1,6 +1,36 @@
 #!/bin/sh
+
+export GST_DEBUG=2
+
+LOGFILENAME=/tmp/`date +"%y%m%d-%H%M%S"`.log
+
+stop_execution()
+{
+	echo "********************  SESSION FINISH (`date`)  ********************" >> ${LOGFILENAME}
+	unset GST_DEBUG
+
+	exit 1	
+}
+
+control_c()
+{
+	echo "********************  SESSION FINISH (`date`)  ********************" >> ${LOGFILENAME}
+	unset GST_DEBUG
+	
+	exit 0
+}
+
+echo "-------------------- SESSION START (`date`) --------------------" >> ${LOGFILENAME}
+
+trap control_c SIGINT
+
 while true
 do
-videosource --config-path=/etc/videosource/vsource.conf
-sleep 2
+	echo `date +"%H:%M:%S.%N"` " - Raising Source" >> ${LOGFILENAME}
+	videosource --config-path=/etc/videosource/vsource.conf 2>> ${LOGFILENAME} 
+	
+	if [ -f /tmp/vs.flag ];
+	then
+		stop_execution	
+	fi
 done
